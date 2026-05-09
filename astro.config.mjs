20d7bdec4398702992e825f79528cea26e6bcb7f
kamitte <3ma510.96ki@gmail.com>
2026-05-09 14:12:13 +0900

    構造化データを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://badminton-lp.pages.dev',
   vite: {
     plugins: [tailwindcss()],
   },
